5a2529520bc01121589d4f23fff696eb5fa4d4a1
Luiz Henrique <luizhjramos@outlook.com>
2023-04-02 19:32:50 -0300

    feat: update product functionality

--- main.go
+++ main.go
@@ -33,6 +33,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	product.Price = 12990.90
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -43,6 +49,20 @@ func insertProduct(db *sql.DB, product *Product) error {
 	defer stmt.Close()
 
 	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
 	if err != nil {
 		panic(err)
 	}
